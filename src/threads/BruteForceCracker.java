b1e3fb12c4fde3bdb70f9a200b9e33a8cd0cb335
plooba <league@WTS13.local>
2017-12-08 17:55:01 -0800

    i have completed module four
    despite keith’s illegitimate claims that I must take a test to
    officially complete this module in question, I have completed what was
    assigned to me at the beginning of this module and thus believe that
    this module is now behind me.

--- src/threads/BruteForceCracker.java
+++ src/threads/BruteForceCracker.java
@@ -11,28 +11,51 @@ package threads;
 import java.util.Random;
 
 public class BruteForceCracker {
-	static long code = (long) (new Random().nextDouble() * 1_000_000_000);
+	static long code = (long) (new Random().nextDouble() * Integer.MAX_VALUE);
 	static long startTime;
 	static long endTime;
 	static float elapsedTime;
-
+	static Thread[] threads = new Thread[20];
 	public static void main(String[] args) {
-		code = 000010000;
+		boolean unsolved = true;
+		for(int i = 0; i < 20; i++) {
+			int j = i;
+			threads[i] = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					startThread(j*100000000);
+				}} );
+		}
+		for(int i = 0; i < 20; i++) {
+			threads[i].start();
+		}
 		System.out.println("Starting Brute Force Checker");
 		startTime = System.currentTimeMillis();
-
-		int ctr = 0;
-		while (!checkCode(ctr++))
+		
+	}
+	public static void startThread(int start) {
+		while (!checkCode(start++))
 			;
-
+	}
+	public static void finish() {
 		endTime = System.currentTimeMillis();
 		elapsedTime = (float) (endTime - startTime);
 		elapsedTime /= 1000.f;
 		System.out.println("Total time taken: " + elapsedTime + " seconds");
+		for(int i = 0; i < 20; i++) {
+			try {
+				threads[i].join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 	}
-
 	public static boolean checkCode(long p) {
 		if (p == code) {
+			System.out.println(code);
+			finish();
 			return true;
 		} else {
 			return false;
